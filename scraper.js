d4b896e2aa73ef7f82e0799df318678617c70ff5
nickfw86 <nickwatson@nicks-mbp.lan>
2020-04-17 12:05:27 -0400

    added logic to tell if product is out of stock, in stock, availble, or expted to be available at given date.

--- scraper.js
+++ scraper.js
@@ -1,17 +1,28 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 require('dotenv').config()
-const url = process.env.TARGET_URL;
 let products = require('./products.js');
+let url = products[0].URL;
 
 axios.get(url)
   .then(function (response) {
     // handle success
     const html = response.data
     const $ = cheerio.load(html);
-    const stockNoteBackorder = $(".stock-note.backordered");
-    const dateAvailable = stockNoteBackorder[0].children[0].data;
-    console.log(dateAvailable);
+    const stockNoteContainer= $(".stock-note-container");
+    const soldOut = $("p:contains(Item currently sold out)");
+
+
+    if (soldOut.length > 0){
+        console.log("Out Of Stock");
+    } else if (typeof stockNoteContainer[0].children[1] === 'object'){
+        let availability = stockNoteContainer[0].children[1].children[0].data;
+        console.log(availability);
+    } else {
+        console.log("Available");
+    }
+
+
   })
   .catch(function (error) {
     // handle error
