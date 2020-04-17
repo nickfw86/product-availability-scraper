c86b944c14b3fd5b44d46d57561fe0e14c31fb01
nickfw86 <nickwatson@nicks-mbp.lan>
2020-04-17 12:09:03 -0400

    format scraper.js

--- scraper.js
+++ scraper.js
@@ -1,28 +1,26 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-require('dotenv').config()
-let products = require('./products.js');
+require("dotenv").config();
+let products = require("./products.js");
 let url = products[0].URL;
 
-axios.get(url)
+axios
+  .get(url)
   .then(function (response) {
     // handle success
-    const html = response.data
+    const html = response.data;
     const $ = cheerio.load(html);
-    const stockNoteContainer= $(".stock-note-container");
+    const stockNoteContainer = $(".stock-note-container");
     const soldOut = $("p:contains(Item currently sold out)");
 
-
-    if (soldOut.length > 0){
-        console.log("Out Of Stock");
-    } else if (typeof stockNoteContainer[0].children[1] === 'object'){
-        let availability = stockNoteContainer[0].children[1].children[0].data;
-        console.log(availability);
+    if (soldOut.length > 0) {
+      console.log("Out Of Stock");
+    } else if (typeof stockNoteContainer[0].children[1] === "object") {
+      let availability = stockNoteContainer[0].children[1].children[0].data;
+      console.log(availability);
     } else {
-        console.log("Available");
+      console.log("Available");
     }
-
-
   })
   .catch(function (error) {
     // handle error
